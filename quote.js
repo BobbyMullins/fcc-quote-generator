4400f234745e6c375c5aacabbc546d543ae27f60
Robert Mullins <robertmullins@192.168.1.2>
2017-08-07 15:19:04 -0400

    first commit

---
+++ quote.js
@@ -0,0 +1,7 @@
+var quotes = ['But man is not made for defeat. A man can be destroyed but not defeated. -Ernest Hemingway', 'There is nothing permanent except change. -Heraclitus', 'Learning never exhausts the mind. -Leonardo da Vinci', 'All that we see or seem is but a dream within a dream. -Edgar Allan Poe', 'The only journey is the one within. -Rainer Maria Rilke']; 
+
+function newQuote() {
+    var randomNumber = Math.floor(Math.random() * (quotes.length)); 
+    document.getElementById('quote-content').innerHTML = quotes[randomNumber]; 
+}
+
